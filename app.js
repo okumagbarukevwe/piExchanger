17a1c3b9cc5d83407ac04a85340162a7b30e72d2
okumagbarukevwe <62697384+okumagbarukevwe@users.noreply.github.com>
2024-10-30 06:00:45 +0100

    Update app.js
    Add console log message

--- app.js
+++ app.js
@@ -65,7 +65,8 @@ fs.readFile('Mail2.html','UTF-8',function(err, data) {
         if(err)
           console.log(err)
         else
-          console.log(info)
+          console.log(info);
+          console.log(message);
           res.redirect('../wallet')
         })
 }) 
